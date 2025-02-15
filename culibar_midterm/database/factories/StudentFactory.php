de2a0598c3624b62471c7d61966c02b9f0966b88
AkosiJinwoo <balondoytisoy@gmail.com>
2025-02-15 20:51:36 +0800

    done admin

---
+++ culibar_midterm/database/factories/StudentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    protected $model = Student::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'student_id' => $this->faker->unique()->numberBetween(100000, 999999),
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+        ];
+    }
+}
--- culibar_midterm/database/seeders/DatabaseSeeder.php
+++ culibar_midterm/database/seeders/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(StudentSeeder::class);
 
         User::factory()->create([
+            'student_id' => 100000,
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
