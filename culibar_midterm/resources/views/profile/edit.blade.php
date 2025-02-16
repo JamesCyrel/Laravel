7f74db121549b10b1cef101f6581d69ea83a34cc
AkosiJinwoo <balondoytisoy@gmail.com>
2025-02-16 18:02:21 +0800

    finale

--- culibar_midterm/resources/views/profile/edit.blade.php
+++ culibar_midterm/resources/views/profile/edit.blade.php
@@ -1,29 +1,32 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.dashboardTemplate')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+@section('title', 'Edit Profile')
+
+@section('content')
+<div class="container">
+    <h1 class="my-4">Edit Profile</h1>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card mb-4">
+                <div class="card-body">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+        </div>
+        <div class="col-md-6">
+            <div class="card mb-4">
+                <div class="card-body">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+        </div>
+        <div class="col-md-12">
+            <div class="card mb-4">
+                <div class="card-body">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+</div>
+@endsection
