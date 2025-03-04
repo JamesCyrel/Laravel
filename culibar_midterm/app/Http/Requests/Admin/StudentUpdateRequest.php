ea3850f289280f1b5a634e6ccb17837079af5055
AkosiJinwoo <balondoytisoy@gmail.com>
2025-03-04 16:13:07 +0800

    student request

--- culibar_midterm/app/Http/Requests/Admin/StudentUpdateRequest.php
+++ culibar_midterm/app/Http/Requests/Admin/StudentUpdateRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Student;
+namespace App\Http\Requests\Admin;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -17,6 +17,7 @@ class StudentUpdateRequest extends FormRequest
             'name' => 'required',
             'email' => 'required|email|unique:students,email,' . $this->route('student')->id,
             'course' => 'required|string',
+            'year_level' => 'required|string',
         ];
     }
 }
